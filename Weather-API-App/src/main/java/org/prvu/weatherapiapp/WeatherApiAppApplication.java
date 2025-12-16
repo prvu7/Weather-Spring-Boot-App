e6cb52f8395107475abd72f3d87ff3656196236f
George-Cristian Pîrvu <george.pirvuu@gmail.com>
2025-12-16 23:01:30 +0200

    Created spring boot project

---
+++ Weather-API-App/src/main/java/org/prvu/weatherapiapp/WeatherApiAppApplication.java
@@ -0,0 +1,13 @@
+package org.prvu.weatherapiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherApiAppApplication.class, args);
+    }
+
+}
---
+++ Weather-API-App/src/test/java/org/prvu/weatherapiapp/WeatherApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prvu.weatherapiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
